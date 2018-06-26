68b603d9ef22c46f22cedaeaaf776cc56ccdfcaf
lokal-profil <lokal.profil@gmail.com>
2018-06-26 14:06:12 +0200

    Convenience function

---
+++ prepUpload.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+"""Prepare files for upload by creating Information pages and renaming them."""
+import batchupload.prepUpload as prep
+
+
+def main(*arguments):
+    prep.main(*arguments)
+
+
+if __name__ == "__main__":
+    main()
